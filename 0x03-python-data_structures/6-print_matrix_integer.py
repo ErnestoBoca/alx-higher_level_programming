fb33dc3de4830fa91d6918ced5c7e6e0bae533bd
Ernesto Boca <boca.ernesto13@gmail.com>
2023-11-06 15:18:51 -0800

    TAsk 6 bug fix

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -1,6 +1,9 @@
-#!/usr/bin/python3
 def print_matrix_integer(matrix=[[]]):
-    for i in range(0, len(matrix)):
-        for num in matrix[i]:
-            print("{:d}".format(num), end=" ")
+    for row in matrix:
+        for i in range(len(row)):
+            if (i == len(row) - 1):
+                end_c = ""
+            else:
+                end_c = " "
+            print("{:d}".format(row[i]), end=end_c)
         print()
