49edc2a7a90ad31ae9394b27387be13b8942e7b5
Ernesto BOca <boca.ernesto13@gmail.com>
2023-12-28 14:08:54 -0800

    Task 2 feat

---
+++ 0x06-python-classes/2-square.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""Defines a Square Class"""
+
+
+class Square:
+    """The Square class"""
+    def __init__(self, size=0):
+        """Initializes a Square with its size
+        Args:
+            self: the object
+            size(int): the size of the square
+        """
+        if (type(size)) == int:
+            if (size >= 0):
+                self.__size = size
+            else:
+                raise ValueError("size must be >= 0")
+        else:
+            raise TypeError("size must be an integer")
