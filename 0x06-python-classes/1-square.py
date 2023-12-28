6182367db847501f6a704c3c61324586c6387642
Ernesto BOca <boca.ernesto13@gmail.com>
2023-12-28 13:56:24 -0800

    feat: task 1

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""Defines a Square Class"""
+
+
+class Square:
+    """The Square class"""
+    def __init__(self, size):
+        """Initializes a Square with its size
+        Args:
+            self: the object
+            size(int): the size of the square
+        """
+        self.__size = size
