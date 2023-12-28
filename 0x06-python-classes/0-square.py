e526c0ce7b347553aff5e1be3b4586dbe6bfb5c8
Ernesto BOca <boca.ernesto13@gmail.com>
2023-12-28 13:27:44 -0800

    fix: task 0

--- 0x06-python-classes/0-square.py
+++ 0x06-python-classes/0-square.py
@@ -2,5 +2,5 @@
 
 """"This module contains an empty class Square"""
 class Square:
-    "This class does nothing for now, it will be upgraded later"
+    """This class does nothing for now, it will be upgraded later"""
     pass
