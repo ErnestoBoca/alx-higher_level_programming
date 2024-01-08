f6515fb518efc6f2a66fff68bab52acfcb9b10ea
Ernesto BOca <boca.ernesto13@gmail.com>
2024-01-08 14:05:04 -0800

    Task 6

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""My Geometry module"""
+
+
+class BaseGeometry:
+    """The Base Class"""
+    pass
+
+    def area(self):
+        """Just raises an exception for now"""
+        raise Exception("area() is not implemented")
