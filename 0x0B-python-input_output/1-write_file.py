1d4bfa66e6ceaaa34507b4ee55ee169e662ba8bd
Ernesto BOca <boca.ernesto13@gmail.com>
2024-01-09 11:37:32 -0800

    Task 1

--- 0x0B-python-input_output/1-write_file.py
+++ 0x0B-python-input_output/1-write_file.py
@@ -3,5 +3,6 @@
 
 
 def write_file(filename="", text=""):
+    """"Writes tect into a file and returns the number of characters written"""
     with open(filename, mode="w", encoding="utf-8") as my_file:
         return my_file.write(text)
