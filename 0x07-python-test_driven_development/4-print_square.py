2e73ff731bef5657a0b5bb320efa368973477d89
Ernesto BOca <boca.ernesto13@gmail.com>
2024-01-11 08:48:00 -0800

    Task 4

---
+++ 0x07-python-test_driven_development/4-print_square.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+"""
+this module contains the print_square function
+"""
+
+
+def print_square(size):
+    """  prints a square with the character #."""
+    if type(size) is not int:
+        raise TypeError("size must be an integer")
+    if size < 0:
+        raise ValueError("size must be >= 0")
+    for i in range(size):
+        for j in range(size):
+            print("#", end="")
+        print()
+
+
+if __name__ == "__main__":
+    import doctest
+    doctest.testfile("./tests/3-say_my_name.txt")
