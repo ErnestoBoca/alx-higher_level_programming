44df97aa1b7300eb94230a3683ae263dabb7d8da
Ernesto Boca <boca.ernesto13@gmail.com>
2024-03-17 14:08:15 -0700

    trying to fix check 2

--- 0x0F-python-object_relational_mapping/model_state.py
+++ 0x0F-python-object_relational_mapping/model_state.py
@@ -16,7 +16,5 @@ class State(Base):
     The states class that creats the states table
     """
     __tablename__ = 'states'
-
-    id = Column(Integer, primary_key=True, unique=True,
-                nullable=False, autoincrement=True)
+    id = Column(Integer, unique=True, nullable=False, primary_key=True)
     name = Column(String(128), nullable=False)
