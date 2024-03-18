fd1fea384cd49fefbdd057b47f71d78eb977152f
Ernesto Boca <boca.ernesto13@gmail.com>
2024-03-18 14:36:19 -0700

    Task 16

---
+++ 0x0F-python-object_relational_mapping/101-relationship_states_cities_list.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+""" creates a state and a city """
+import sys
+from relationship_state import Base, State
+from relationship_city import City
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+
+
+def execute():
+    user = sys.argv[1]
+    pswd = sys.argv[2]
+    db = sys.argv[3]
+
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost:3306/{}'
+                           .format(user, pswd, db))
+    Session = sessionmaker(bind=engine)
+    session = Session()
+
+    states = session.query(State).join(City).order_by(State.id, City.id).all()
+
+    for state in states:
+        print('{}: {}'.format(state.id, state.name))
+        for city in state.cities:
+            print('\t{}:{}'.format(city.id, city.name))
+
+
+if __name__ == '__main__':
+    execute()
