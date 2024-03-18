08481fbaff4ce161fbbeef4f869f456c3230c145
Ernesto Boca <boca.ernesto13@gmail.com>
2024-03-18 06:19:16 -0700

    fixing task 8

--- 0x0F-python-object_relational_mapping/8-model_state_fetch_first.py
+++ 0x0F-python-object_relational_mapping/8-model_state_fetch_first.py
@@ -19,9 +19,10 @@ def execute():
     session = Session()
 
     state = session.query(State).first()
-
-    print('{}: {}'.format(state.id, state.name))
-
+    if state is not None:
+        print('{}: {}'.format(state.id, state.name))
+    else:
+        print()
 
 if __name__ == '__main__':
     execute()
