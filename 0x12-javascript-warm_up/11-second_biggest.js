812c7055ed04d2b7cd4dcb7eb1d55dffcef0d6f2
Ernesto Boca <boca.ernesto13@gmail.com>
2024-04-08 15:29:00 -0700

    task 11 fix

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -3,7 +3,7 @@
 if (process.argv.length <= 3) console.log('0');
 else {
   let biggest = parseInt(process.argv[2]);
-  let secBiggest = parseInt(process.argv[2]);
+  let secBiggest = -Infinity;
   for (let i = 3; i < process.argv.length; i++) {
     if (process.argv[i] > biggest) {
       secBiggest = biggest;
