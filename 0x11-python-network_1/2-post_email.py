8644acc605a9288f4b7580e4681f829a00e55ee2
Ernesto Boca <boca.ernesto13@gmail.com>
2024-07-27 12:50:44 -0700

    task 2 fix

--- 0x11-python-network_1/2-post_email.py
+++ 0x11-python-network_1/2-post_email.py
@@ -2,7 +2,8 @@
 """This script that takes in a URL and an email, sends a POST request
 to the passed URL with the email as a parameter, and displays the body
 of the response (decoded in utf-8)"""
-import urllib.request, urllib.parse
+import urllib.request
+import urllib.parse
 import sys
 
 
